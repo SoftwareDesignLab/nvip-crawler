cc1b95587dff8d9e13bac301aaf9ce140f538453
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-04-20 23:22:34 -0400

    fixed issue with NVD and MITRE gaps not being pulled

--- src/main/java/edu/rit/se/nvip/db/DbParallelProcessor.java
+++ src/main/java/edu/rit/se/nvip/db/DbParallelProcessor.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package edu.rit.se.nvip.cvereconcile.db;
+package edu.rit.se.nvip.db;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
