f9f1e19ea60dc02f5c30fc9dd3515abadf414cd2
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-04-30 23:00:08 -0400

    liquibase for vuln db

--- nvip_data/mysql-database/newDB/DBCVSS.sql
+++ nvip_data/mysql-database/newDB/DBCVSS.sql
@@ -8,6 +8,6 @@ CREATE TABLE `cvssscore` (
   `create_date` DATETIME NOT NULL,
   `base_score` DOUBLE NOT NULL,
   `impact_score` DOUBLE NOT NULL,
-  KEY `CveId` (`cve_id`),
+  KEY `cvss_cve_id_index` (`cve_id`),
   CONSTRAINT `cvssscore_cve_id_fk` FOREIGN KEY (`cve_id`) REFERENCES `vulnerability` (`cve_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
