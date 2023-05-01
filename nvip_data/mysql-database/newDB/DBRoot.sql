db81b001758a798d0e506a8fd5739e0036126112
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-04-30 23:00:08 -0400

    liquibase for vuln db

--- nvip_data/mysql-database/newDB/DBRoot.sql
+++ nvip_data/mysql-database/newDB/DBRoot.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE IF NOT EXISTS nvip_new
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_general_ci;
-
-USE nvip_new;
-
--- Needed for parallel db processes
-SET GLOBAL max_connections = 600;
--- Disable Group By requirement for aggregations
-SET GLOBAL sql_mode="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION";
+-- CREATE DATABASE IF NOT EXISTS nvip_new
+-- CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_general_ci;
+-- 
+-- USE nvip_new;
+-- 
+-- -- Needed for parallel db processes
+-- SET GLOBAL max_connections = 600;
+-- -- Disable Group By requirement for aggregations
+-- SET GLOBAL sql_mode="STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION";
 
 --
 -- Table structure for table `vulnerability`
@@ -24,8 +24,7 @@ CREATE TABLE `vulnerability` (
   `status` TINYTEXT NOT NULL,
   PRIMARY KEY (`vuln_id`),
   UNIQUE KEY `vuln_id_UNIQUE` (`vuln_id`),
-  KEY `Vulnerability_Index_CveId` (`cve_id`),
-  KEY `status_id_idx` (`status_id`)
+  KEY `vulnerability_index_cve_id` (`cve_id`)
 ) 
 ENGINE=InnoDB AUTO_INCREMENT=2157992 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
