fdbf3f8c0eb87cf6067edbfa5631a30a0e89eef2
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-05-04 15:31:32 -0400

    Added check for duplicate entries in rawdescription table

--- crawler/src/main/java/edu/rit/se/nvip/CrawlerMain.java
+++ crawler/src/main/java/edu/rit/se/nvip/CrawlerMain.java
@@ -91,7 +91,7 @@ public class CrawlerMain {
 
         for (String cveId: crawledCves.keySet()) {
             for (RawVulnerability vuln: crawledCves.get(cveId)) {
-                if (!databaseHelper.checkIfInRawDescriptions(vuln.getDescription())) {
+                if (!databaseHelper.checkIfInRawDescriptions(vuln.getCveId(), vuln.getDescription())) {
                     logger.info("Inserting new raw description for CVE {} into DB" ,cveId);
                     insertedCVEs += databaseHelper.insertRawVulnerability(vuln);
                     databaseHelper.addJobForCVE(vuln.getCveId());
