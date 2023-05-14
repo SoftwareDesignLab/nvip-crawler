932b49efaa73339cf8e7af73720d4004b44359cd
Corey Urbanke <cju8676@rit.edu>
2023-05-14 15:11:49 -0400

    added ParseBulletinTests, added GenericDate to ParseTable

--- src/main/java/edu/rit/se/nvip/crawler/htmlparser/ParseBulletin.java
+++ src/main/java/edu/rit/se/nvip/crawler/htmlparser/ParseBulletin.java
@@ -2,12 +2,21 @@ package edu.rit.se.nvip.crawler.htmlparser;
 
 import edu.rit.se.nvip.model.CompositeVulnerability;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ParseBulletin extends AbstractCveParser implements ParserStrategy {
 
+    public ParseBulletin(String sourceDomainName) {
+        this.sourceDomainName = sourceDomainName;
+    }
+
     @Override
     public List<CompositeVulnerability> parseWebPage(String sSourceURL, String sCVEContentHTML) {
-        return null;
+        List<CompositeVulnerability> vulnList = new ArrayList<>();
+
+
+
+        return vulnList;
     }
 }
