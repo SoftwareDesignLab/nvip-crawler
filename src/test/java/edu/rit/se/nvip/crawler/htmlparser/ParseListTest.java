b776287fe51e453fec83bff59bfc9edc4b460526
Andromeda Sawtelle <andromeda.sawtelle@gmail.com>
2023-05-15 13:16:48 -0400

    Added ParseListTest and HTMLs for testing

---
+++ src/test/java/edu/rit/se/nvip/crawler/htmlparser/ParseListTest.java
@@ -0,0 +1,60 @@
+package edu.rit.se.nvip.crawler.htmlparser;
+
+import edu.rit.se.nvip.model.CompositeVulnerability;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParseListTest extends AbstractParserTest {
+
+    @Test
+    public void testParseListSwift() {
+        String html = safeReadHtml("src/test/resources/test-generic_list_parser-swift.html");
+        ParseList parser = new ParseList("https://www.swift.org/support/security.html");
+        List<CompositeVulnerability> list = parser.parseWebPage(
+                "https://www.swift.org/support/security.html",
+                html
+        );
+
+        assertTrue(list.size() > 9);
+        CompositeVulnerability vuln = getVulnerability(list, "CVE-2022-24666");
+        assertNotNull(vuln);
+        assertEquals("", vuln.getPublishDate());
+        assertTrue(vuln.getDescription().contains("A program using swift-nio-http2 is vulnerable to a denial of service attack"));
+    }
+
+    @Test
+    public void testParseListNaver() {
+        String html = safeReadHtml("src/test/resources/test-generic_list_parser-naver.html");
+        ParseList parser = new ParseList("https://cve.naver.com/advisory");
+        List<CompositeVulnerability> list = parser.parseWebPage(
+                "https://cve.naver.com/advisory",
+                html
+        );
+
+        assertTrue(list.size() > 19);
+        CompositeVulnerability vuln = getVulnerability(list, "CVE-2022-24077");
+        assertNotNull(vuln);
+        assertEquals("", vuln.getPublishDate());
+        assertTrue(vuln.getDescription().contains("Naver Cloud Explorer Beta allows the attacker to execute arbitrary code"));
+    }
+
+    @Test
+    public void testParseListOpenSSL() {
+        String html = safeReadHtml("src/test/resources/test-generic_list_parser-openssl.html");
+        ParseList parser = new ParseList("https://www.openssl.org/news/vulnerabilities.html");
+        List<CompositeVulnerability> list = parser.parseWebPage(
+                "https://www.openssl.org/news/vulnerabilities.html",
+                html
+        );
+
+        assertTrue(list.size() > 190);
+        CompositeVulnerability vuln = getVulnerability(list, "CVE-2023-0465");
+        assertNotNull(vuln);
+        assertEquals("23 March 2023", vuln.getPublishDate());
+        assertTrue(vuln.getDescription().contains("Applications that use a non-default option when verifying certificates may be vulnerable "));
+    }
+}
