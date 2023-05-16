dcf4ef00987e1faa5cf718b5a06d4c7d999eac28
memeeerit <memeee@rit.edu>
2023-05-16 15:50:47 -0400

    moved a couple methods out of the databasehelper and into a new DatabaseSandbox class in a new sandbox package. This package is where any development tools that don't need testing should go. TODO alter the dockerfile and pom.xml to skip this package during compilation.

--- reconciler/src/main/java/edu/rit/se/nvip/DatabaseHelper.java
+++ reconciler/src/main/java/edu/rit/se/nvip/DatabaseHelper.java
@@ -54,7 +54,7 @@ public class DatabaseHelper {
         return databaseHelper;
     }
 
-    private DatabaseHelper(HikariConfig config) {
+    protected DatabaseHelper(HikariConfig config) {
         try {
             logger.info("New NVIP.DatabaseHelper instantiated! It is configured to use " + databaseType + " database!");
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -73,7 +73,7 @@ public class DatabaseHelper {
         }
     }
 
-    private static HikariConfig createHikariConfigFromArgs(String url, String username, String password) {
+    protected static HikariConfig createHikariConfigFromArgs(String url, String username, String password) {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(url);
         hikariConfig.setUsername(username);
@@ -81,7 +81,7 @@ public class DatabaseHelper {
         return hikariConfig;
     }
 
-    private static HikariConfig createHikariConfigFromEnvironment() {
+    protected static HikariConfig createHikariConfigFromEnvironment() {
 
         String url = System.getenv("HIKARI_URL");
         HikariConfig hikariConfig;
@@ -338,59 +338,5 @@ public class DatabaseHelper {
             logger.error(ex);
         }
         return rawVuln;
-    }
-
-
-    /**
-     * just for some informal sandbox testing, look away
-     * @param rawVulns
-     */
-    public void insertForTest(List<RawVulnerability> rawVulns) {
-        String query = "INSERT INTO rawdescription (cve_id, raw_description, created_date, published_date, last_modified_date, source_url) VALUES (?, ?, ?, ?, ?, ?)";
-        String query2 = "INSERT INTO cvejobtrack (cve_id) VALUES (?)";
-        Set<String> jobbedCves = new HashSet<>();
-        try (Connection conn = getConnection(); PreparedStatement pstmt = conn.prepareStatement(query); PreparedStatement pstmt2 = conn.prepareStatement(query2);
-        PreparedStatement delete1 = conn.prepareStatement("DELETE FROM cvejobtrack"); PreparedStatement delete2 = conn.prepareStatement("DELETE FROM rawdescription")) {
-            delete1.executeUpdate();
-            delete2.executeUpdate();
-
-            for (RawVulnerability vuln : rawVulns) {
-                pstmt.setString(1, vuln.getCveId());
-                pstmt.setString(2, vuln.getDescription());
-                pstmt.setTimestamp(3, vuln.getCreateDate());
-                pstmt.setTimestamp(4, vuln.getPublishDate());
-                pstmt.setTimestamp(5, vuln.getLastModifiedDate());
-                pstmt.setString(6, vuln.getSourceUrl());
-                pstmt.addBatch();
-
-                if (!jobbedCves.contains(vuln.getCveId())) {
-                    pstmt2.setString(1, vuln.getCveId());
-                    pstmt2.addBatch();
-                    jobbedCves.add(vuln.getCveId());
-                }
-            }
-            pstmt.executeBatch();
-            pstmt2.executeUpdate();
-
-        } catch (SQLException ex) {
-            logger.error(ex);
-        }
-    }
-
-    public void insertRawVuln(RawVulnerability vuln) {
-        String query = "INSERT INTO rawdescription (raw_description, created_date, published_date, last_modified_date, source_url, cve_id) VALUES (?, ?, ?, ?, ?, ?)";
-
-
-        try (Connection conn = getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
-            pstmt.setString(1, vuln.getDescription());
-            pstmt.setTimestamp(2, vuln.getCreateDate());
-            pstmt.setTimestamp(3, vuln.getPublishDate());
-            pstmt.setTimestamp(4, vuln.getLastModifiedDate());
-            pstmt.setString(5, vuln.getSourceUrl());
-            pstmt.setString(6, vuln.getCveId());
-            pstmt.executeUpdate();
-        } catch (SQLException ex) {
-            logger.error(ex);
-        }
     }
 }
