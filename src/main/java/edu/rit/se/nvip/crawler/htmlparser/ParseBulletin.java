1ab73d9fd03dee9b8d8ffeb2fae854ab1be41364
Corey Urbanke <cju8676@rit.edu>
2023-05-18 11:20:15 -0400

    added extra date checks to ParseTable
    - check for created / last modified text to distinguish
    - use create date (today) if date is empty or null

--- src/main/java/edu/rit/se/nvip/crawler/htmlparser/ParseBulletin.java
+++ src/main/java/edu/rit/se/nvip/crawler/htmlparser/ParseBulletin.java
@@ -15,55 +15,6 @@ public class ParseBulletin extends AbstractCveParser implements ParserStrategy {
         this.sourceDomainName = sourceDomainName;
     }
 
-    /**
-     * Extract date helper - get substring bounds
-     * @return array of substring bounds
-     */
-    private int[] getSubstringBounds(String text, String keyword) {
-        // bounds to isolate date text for individual CVE ID's in bulletin
-        final int DATE_BOUNDS = 40;
-
-        int[] bounds = new int[2];
-        int keywordIndex = text.toLowerCase().indexOf(keyword);
-        if (keywordIndex == -1) return bounds;
-        bounds[0] = keywordIndex;
-        bounds[1] = Math.min(keywordIndex + DATE_BOUNDS, text.length());
-        return bounds;
-    }
-
-    // search for date keywords, grab dates around it
-    private GenericDate extractDate(String text) {
-        // search for "Published" "Created" "Modified" "Updated" keywords, grab dates around it
-        // check a subtext for a date based on these keywords
-        if (text.toLowerCase().contains("published")) {
-            // grab date around published
-            int[] bounds = getSubstringBounds(text, "published");
-            return new GenericDate(text.substring(bounds[0], bounds[1]));
-        } else if (text.toLowerCase().contains("created")) {
-            // grab date around created
-            int[] bounds = getSubstringBounds(text, "created");
-            return new GenericDate(text.substring(bounds[0], bounds[1]));
-        }
-        // otherwise try to find any sort of date in the text (this might give back rogue dates in descriptions, etc...)
-        return new GenericDate(text);
-    }
-
-    private GenericDate extractLastModifiedDate(String text) {
-        // search for "Published" "Created" "Modified" "Updated" keywords, grab dates around it
-        // check a subtext for a date based on these keywords
-        if (text.toLowerCase().contains("modified")) {
-            // grab date around modified
-            int[] bounds = getSubstringBounds(text, "modified");
-            return new GenericDate(text.substring(bounds[0], bounds[1]));
-        } else if (text.toLowerCase().contains("updated")) {
-            // grab date around updated
-            int[] bounds = getSubstringBounds(text, "updated");
-            return new GenericDate(text.substring(bounds[0], bounds[1]));
-        }
-        // otherwise try to find any sort of date in the text (this might give back rogue dates in descriptions, etc...)
-        return new GenericDate(text);
-    }
-
     @Override
     public List<CompositeVulnerability> parseWebPage(String sSourceURL, String sCVEContentHTML) {
         // bounds to grab relevant description text for bulletins
