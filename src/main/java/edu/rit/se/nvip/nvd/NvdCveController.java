c81760899a3685bb6b8ff4fb15b7e88b2ff00010
Andrew Pickard <59809184+aep7128@users.noreply.github.com>
2023-05-22 16:13:07 -0400

    NVDController now pulls most recent to earliest, # of requests is function parameter

--- src/main/java/edu/rit/se/nvip/nvd/NvdCveController.java
+++ src/main/java/edu/rit/se/nvip/nvd/NvdCveController.java
@@ -69,6 +69,8 @@ public class NvdCveController {
 	private String startDate;
 	private String endDate;
 
+	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss");
+
 	/**
 	 * for testing NVD CVE pull
 	 * TODO: timeout exception when in middle of pulling NVD CVEs,
@@ -77,9 +79,10 @@ public class NvdCveController {
 	 */
 	public static void main(String[] args) {
 		NvdCveController nvd = new NvdCveController();
-		HashMap<String, NvdVulnerability> nvdCves = nvd.fetchNVDCVEs("https://services.nvd.nist.gov/rest/json/cves/2.0");
+		HashMap<String, NvdVulnerability> nvdCves = nvd.fetchNVDCVEs(
+				"https://services.nvd.nist.gov/rest/json/cves/2.0?pubstartDate=<StartDate>&pubEndDate=<EndDate>", 10);
 		for (String cve: nvdCves.keySet()) {
-			System.out.println(nvdCves.get(cve));
+			//System.out.println(nvdCves.get(cve));
 		}
 	}
 
@@ -89,8 +92,7 @@ public class NvdCveController {
 	 */
 	public NvdCveController() {
 		LocalDateTime today = LocalDateTime.now();
-		LocalDateTime lastMonth = LocalDateTime.now().minusDays(30);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss");
+		LocalDateTime lastMonth = LocalDateTime.now().minusDays(119);
 
 		this.startDate = lastMonth.format(formatter);
 		this.endDate = today.format(formatter);
@@ -101,19 +103,20 @@ public class NvdCveController {
 	 * @param nvdApiPath
 	 * @return
 	 */
-	public HashMap<String, NvdVulnerability> fetchNVDCVEs(String nvdApiPath) {
+	public HashMap<String, NvdVulnerability> fetchNVDCVEs(String nvdApiPath, int requestLimit) {
 		HashMap<String, NvdVulnerability> NvdCves = new HashMap<>();
 		int resultsPerPage = 2000;
 		int startIndex = 0;
 		int requestNum = 0;
 
-		while (true) {
-			// 30 second wait for eevry 5 requests, according to NVD Doc: https://nvd.nist.gov/developers/start-here
-			if (requestNum >= 5) {
+		String currentRequestString = nvdApiPath.replaceAll("<StartDate>", this.startDate).replaceAll("<EndDate>", this.endDate);
+
+		while (requestNum < requestLimit) {
+			// 30 second wait for every 5 requests, according to NVD Doc: https://nvd.nist.gov/developers/start-here
+			if (requestNum % 5 == 0 && requestNum > 0) {
 				logger.info("Sleeping for 60 seconds before continuing");
 				try {
 					Thread.sleep(60000);
-					requestNum = 0;
 				} catch (InterruptedException e) {
 					logger.error("ERROR: Failed to wait 30seconds for pulling NVD CVEs\n{}", e);
 				}
@@ -121,17 +124,17 @@ public class NvdCveController {
 
 			try {
 				// Pull from NVD, keep track of startIndex for paginating response
-				String url = nvdApiPath + "?resultsPerPage=" + resultsPerPage + "&startIndex=" + startIndex;
+				String url = currentRequestString + "&resultsPerPage=" + resultsPerPage + "&startIndex=" + startIndex;
 				URL apiUrl = new URL(url);
 				HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
 				connection.setRequestMethod("GET");
 
 				if (connection.getResponseCode() != 200) {
-					logger.error("Error retrieving CVEs: {}\n{}", connection.getResponseCode(), connection.getResponseMessage());
+					logger.error("Error retrieving CVEs with URL {}\nResponse Code: {}\n{}", url, connection.getResponseCode(), connection.getResponseMessage());
 					break;
 				}
 
-				logger.info("Connection Acquired!");
+				logger.info("Connection Acquired for URL {}", url);
 
 				requestNum++;
 
@@ -153,7 +156,6 @@ public class NvdCveController {
 					break;
 				}
 
-				logger.info("Parsing CVEs");
 				// for each vulnerability in response list, add cveId, publishedDate and status to the hashmap
 				for (int i = 0; i < vulnerabilities.length(); i++) {
 					JSONObject cve = vulnerabilities.getJSONObject(i);
@@ -176,8 +178,13 @@ public class NvdCveController {
 				int totalResults = cveData.getInt("totalResults");
 				startIndex += cveData.getInt("resultsPerPage");
 
+				// If we've reached the total results for the response, move the start and end dates back by 119 days
+				// We must adhere to the 120 day range limit in NVD's API for specifying date ranges
 				if (startIndex >= totalResults) {
-					break;
+					startIndex = 0;
+					this.endDate = this.startDate;
+					this.startDate = LocalDateTime.parse(this.endDate).minusDays(119).format(formatter);
+					currentRequestString = nvdApiPath.replaceAll("<StartDate>", this.startDate).replaceAll("<EndDate>", this.endDate);
 				}
 
 				connection.disconnect();
