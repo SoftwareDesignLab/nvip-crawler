e11d0e1510a13b0d1a712dfa16f750fe9748aa90
jqm4954 <jqm4954@rit.edu>
2023-05-25 14:59:51 -0400

    Added setNotGarbage method in DatabaseSandbox and added functionality for setting garbage in LabelingTool

--- reconciler/src/main/java/edu/rit/se/nvip/sandbox/LabelingTool.java
+++ reconciler/src/main/java/edu/rit/se/nvip/sandbox/LabelingTool.java
@@ -1,24 +1,39 @@
 package edu.rit.se.nvip.sandbox;
 
+import edu.rit.se.nvip.DatabaseHelper;
 import edu.rit.se.nvip.model.RawVulnerability;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LabelingTool {
     public void runLabelingTool() {
         System.out.println("LABELING TOOL FOR GENERIC PARSER DATA INPUT (Input from `rawdescriptions` table in DB)");
 
         Scanner scan = new Scanner(System.in);
-
         System.out.println("How many descriptions would you like to assign? (Type ALL for all desc. in `rawdescriptions` or enter num)");
-        String quant = scan.next();
+        String quantity = scan.next();
         System.out.println();
+
+        //Setup database sandbox
         DatabaseSandbox dbs = DatabaseSandbox.getInstance();
-        LinkedList<RawVulnerability> rawVulnList = dbs.getRawDescriptions(quant);
+        LinkedList<RawVulnerability> rawVulnList = dbs.getRawDescriptions(quantity);
+
+        //Setup database helper
+        DatabaseHelper dbh = DatabaseHelper.getInstance();
+
+        //Create empty rejected
+        Set<RawVulnerability> rejected = new HashSet<>();
+
+        //Create empty accepted list
+        LinkedList<RawVulnerability> accepted = new LinkedList<>();
+
         //Iterate through result set
         while (rawVulnList.size() != 0) {
             RawVulnerability current = rawVulnList.pop();
+
             //Print current result's info
             System.out.println("rawdescription ID: " + current.getId());
             System.out.println("rawdescription Description: " + current.getDescription());
@@ -31,10 +46,15 @@ public class LabelingTool {
             System.out.println("Is CVE Good Quality? (Enter Y or N): ");
             String quality = scan.next();
             System.out.println();
+
             if (quality.equals("Y")) {
-                //Add cve to new table/json/csv
+                accepted.add(current);
+            } else {
+                rejected.add(current);
             }
         }
+        dbs.setNotGarbage(accepted);
+        dbh.markGarbage(rejected);
     }
 
     public static void main(String[] args) {
