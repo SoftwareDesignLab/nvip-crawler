bcb06a2852366faa9c54ef76e826545e92bcbf12
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-06-07 16:18:01 -0400

    Switched String[] -> int[] for easier data manipulation

--- productnameextractor/src/main/java/model/ProductVersion.java
+++ productnameextractor/src/main/java/model/ProductVersion.java
@@ -10,8 +10,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
+/**
+ * Data class to represent a version of a product. This class also contains static functionality
+ * to manipulate ProductVersion instances.
+ */
 public class ProductVersion implements Comparable<ProductVersion> {
-    private final String[] versionParts;
+    private final int[] versionParts;
     // Regex101: https://regex101.com/r/cy9Hp3/1
     private static final Pattern VERSION_PATTERN = Pattern.compile("^((?:[0-9]\\.?)*)$");
     private final static Logger logger = LogManager.getLogger(ProductVersion.class);
@@ -22,10 +26,15 @@ public class ProductVersion implements Comparable<ProductVersion> {
         assert isVersion(versionString);
 
         // Split version into parts
-        this.versionParts = versionString.split("\\.");
+        try {
+            this.versionParts = Arrays.stream(versionString.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        } catch (NumberFormatException e) {
+            logger.error("Failed to create ProductVersion from String '{}'", versionString);
+            throw e;
+        }
     }
 
-    private ProductVersion(String[] versionParts) {
+    private ProductVersion(int[] versionParts) {
         this.versionParts = versionParts;
     }
 
@@ -83,8 +92,8 @@ public class ProductVersion implements Comparable<ProductVersion> {
      * @return A list of interpolated versions between the first and second versions
      */
     public static List<ProductVersion> interpolateVersions(ProductVersion firstVersion, ProductVersion secondVersion, int firstSpec, int secondSpec) {
-        String[] firstParts = firstVersion.versionParts;
-        String[] secondParts = secondVersion.versionParts;
+        int[] firstParts = firstVersion.versionParts;
+        int[] secondParts = secondVersion.versionParts;
 
         List<ProductVersion> interpolatedVersions = new ArrayList<>();
 
@@ -92,33 +101,33 @@ public class ProductVersion implements Comparable<ProductVersion> {
         int[] specCounts = new int[specDifference];
 
         // Differentiate between long and short versions, standardizing the length of both arrays
-        final String[] longerParts;
-        final String[] shorterParts;
+        final int[] longerParts;
+        final int[] shorterParts;
         boolean firstShorter;
         if (firstParts.length > secondParts.length) {
             firstShorter = false;
             longerParts = firstParts;
-            shorterParts = new String[longerParts.length];
+            shorterParts = new int[longerParts.length];
             System.arraycopy(secondParts, 0, shorterParts, 0, secondParts.length);
             secondParts = shorterParts;
         } else {
             firstShorter = true;
             longerParts = secondParts;
-            shorterParts = new String[longerParts.length];
+            shorterParts = new int[longerParts.length];
             System.arraycopy(firstParts, 0, shorterParts, 0, firstParts.length);
             firstParts = shorterParts;
         }
 
         // Calculate specCounts
         for (int i = 0; i < specDifference; i++) {
-            specCounts[i] = roundUpToNearestTen(Integer.parseInt(longerParts[specDifference + i]));
+            specCounts[i] = roundUpToNearestTen(longerParts[specDifference + i]);
             // Fill shorter version with max values (specCounts)
-            if(firstShorter) firstParts[specDifference + i] = String.valueOf(specCounts[i]);
-            else secondParts[specDifference + i] = String.valueOf(specCounts[i]);
+            if(firstShorter) firstParts[specDifference + i] = specCounts[i];
+            else secondParts[specDifference + i] = specCounts[i];
         }
 
         // Interpolate versions with the given version info
-        interpolateVersionsRecursive(interpolatedVersions, firstParts, secondParts, specCounts, new String[Math.max(firstSpec, secondSpec)], specDifference, 0);
+        interpolateVersionsRecursive(interpolatedVersions, firstParts, secondParts, specCounts, new int[Math.max(firstSpec, secondSpec)], specDifference, 0);
 
         // Return list of interpolated versions
         return interpolatedVersions;
@@ -134,47 +143,28 @@ public class ProductVersion implements Comparable<ProductVersion> {
      * @param currentVersion       The current version being constructed
      * @param currentSpec          The current level of specificity
      */
-    private static void interpolateVersionsRecursive(List<ProductVersion> interpolatedVersions, String[] firstParts, String[] secondParts, int[] specCounts, String[] currentVersion, int specDifference, int currentSpec) {
+    private static void interpolateVersionsRecursive(List<ProductVersion> interpolatedVersions, int[] firstParts, int[] secondParts, int[] specCounts, int[] currentVersion, int specDifference, int currentSpec) {
+        // Once version is resolved, add to interpolatedVersions
         if (currentSpec == currentVersion.length) {
             interpolatedVersions.add(new ProductVersion(currentVersion.clone()));
-        } else {
+        } else { // Otherwise, resolve currentSpec
             if (currentSpec < specDifference) {
                 currentVersion[currentSpec] = firstParts[currentSpec];
                 interpolateVersionsRecursive(interpolatedVersions, firstParts, secondParts, specCounts, currentVersion, specDifference, currentSpec + 1);
             } else {
-                int count = specCounts[currentSpec - specDifference];
+                int start = Math.min(firstParts[currentSpec], secondParts[currentSpec]);
+                int end = specCounts[currentSpec - specDifference];
 
-                for (int i = 1; i <= count; i++) {
-                    double ratio = (double) i / count;
-
-                    if (currentSpec < currentVersion.length) {
-
-                        int interpolatedPart = interpolatePart(firstParts[currentSpec], secondParts[currentSpec], ratio);
-                        currentVersion[currentSpec] = Integer.toString(interpolatedPart);
-                        interpolateVersionsRecursive(interpolatedVersions, firstParts, secondParts, specCounts, currentVersion, specDifference, currentSpec + 1);
-                    }
+                for (int i = start; i <= end; i++) {
+                    // If we reach max value, increment preceding element (if there is one) and set current element to 0
+                    if(i == end && currentSpec > 0) {
+                        currentVersion[currentSpec - 1] = currentVersion[currentSpec - 1] + 1;
+                        currentVersion[currentSpec] = 0;
+                    } else currentVersion[currentSpec] = i;
+                    interpolateVersionsRecursive(interpolatedVersions, firstParts, secondParts, specCounts, currentVersion, specDifference, currentSpec + 1);
                 }
             }
         }
-    }
-
-
-    /**
-     * Calculates the number of versions to be interpolated for a specific level of specificity.
-     *
-     * @param versionParts The array of version parts
-     * @param spec         The level of specificity
-     * @return The number of versions to be interpolated
-     */
-    private static int getVersionCount(String[] versionParts, int spec) {
-        if (spec >= versionParts.length) {
-            return 1;
-        }
-
-        String versionPart = versionParts[spec];
-        int partValue = Integer.parseInt(versionPart);
-
-        return Math.max(roundUpToNearestTen(partValue), 1);
     }
 
     /**
@@ -187,36 +177,16 @@ public class ProductVersion implements Comparable<ProductVersion> {
         return (int) (Math.ceil(value / 10.0) * 10);
     }
 
-    /**
-     * Interpolates a version part between the first and second part based on a given ratio.
-     *
-     * @param firstPart  The first version part
-     * @param secondPart The second version part
-     * @param ratio      The interpolation ratio
-     * @return The interpolated version part
-     */
-    private static int interpolatePart(String firstPart, String secondPart, double ratio) {
-        // Define int vars
-        int firstValue, secondValue;
-
-        // Try to parse and default to 0 if fails
-        try { firstValue = Integer.parseInt(firstPart); } catch (NumberFormatException ignored) { firstValue = 0; }
-        try { secondValue = Integer.parseInt(secondPart); } catch (NumberFormatException ignored) { secondValue = 0; }
-
-        // Return interpolated value
-        return (int) (firstValue + (secondValue - firstValue) * ratio);
-    }
-
     @Override
     public int compareTo(@NotNull ProductVersion o) {
         // Extract parts lists
-        String[] parts = this.versionParts;
-        String[] otherParts = o.versionParts;
+        int[] parts = this.versionParts;
+        int[] otherParts = o.versionParts;
         int shortest = Math.min(parts.length, otherParts.length);
         for (int i = 0; i < shortest; i++) {
             // Extract part values
-            int vp = Integer.parseInt(parts[i]);
-            int otherVp = Integer.parseInt(otherParts[i]);
+            int vp = parts[i];
+            int otherVp = otherParts[i];
 
             // If greater/less, return comparison result
             if(vp < otherVp) return -1;
@@ -239,14 +209,13 @@ public class ProductVersion implements Comparable<ProductVersion> {
 
     @Override
     public String toString() {
-        return "ProductVersion{" +
-                "versionParts=" + Arrays.toString(versionParts) +
-                '}';
+        return String.join(".", Arrays.stream(this.versionParts).mapToObj(Integer::toString).toArray(String[]::new));
     }
 
     public static void main(String[] args) {
         ProductVersion v1 = new ProductVersion("1.2.5.13");
         ProductVersion v2 = new ProductVersion("1.3");
+        logger.info("Interpolating versions between {} and {}", v1, v2);
         List<ProductVersion> versions = v1.interpolate(v2);
         versions.forEach(logger::info);
     }
