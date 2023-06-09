ea5919b995bec70737284c5c8187b5992eefad2d
jqm4954 <jqm4954@rit.edu>
2023-06-09 15:26:43 -0400

    rebasing

--- reconciler/src/main/java/edu/rit/se/nvip/filter/FilterFactory.java
+++ reconciler/src/main/java/edu/rit/se/nvip/filter/FilterFactory.java
@@ -32,6 +32,10 @@ public class FilterFactory {
     public static final String BLANK_DESCRIPTION = "BLANK_DESCRIPTION";
     public static final String OPENAI = "OPENAI";
 
+    public static final String INTEGER_DESCRIPTION = "INTEGER_DESCRIPTION";
+
+    public static final String MULTIPLE_CVE_DESCRIPTION = "MULTIPLE_CVE_DESCRIPTION";
+
     public static Filter createFilter(String type) {
         switch (type) {
             case SIMPLE:
@@ -40,6 +44,10 @@ public class FilterFactory {
                 return new CveMatchesDescriptionFilter();
             case BLANK_DESCRIPTION:
                 return new BlankDescriptionFilter();
+            case INTEGER_DESCRIPTION:
+                return new IntegerDescriptionFilter();
+            case MULTIPLE_CVE_DESCRIPTION:
+                return new MultipleCveDescriptionsFilter();
             case OPENAI:
                 return new GPTFilter();
             default:
