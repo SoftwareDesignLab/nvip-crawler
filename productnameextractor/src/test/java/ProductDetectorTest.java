7048c605e7cf8883160f8b32c7561851094df052
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-06-12 14:16:54 -0400

    Stubbed out ProductDetectorTest

---
+++ productnameextractor/src/test/java/ProductDetectorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Class to test ProductDetectorTest
+ *
+ * @author Dylan Mulligan
+ *
+ */
+public class ProductDetectorTest {
+    @Test
+    public void someTest(){
+
+    }
+}
