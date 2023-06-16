370c61a1402c61b491ef2a67cddcf5086793f722
Dylan Mulligan <60295839+dylan-mulligan@users.noreply.github.com>
2023-06-16 13:59:45 -0400

    startingRevision appears to be the troublemaker, updated logging to better reflect what is going wrong

--- patchfinder/src/main/java/commits/PatchCommitScraper.java
+++ patchfinder/src/main/java/commits/PatchCommitScraper.java
@@ -41,16 +41,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.internal.storage.file.WindowCache;
-import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ProgressMonitor;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.patch.Patch;
+import org.eclipse.jgit.lib.*;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.storage.file.WindowCacheConfig;
-import org.eclipse.jgit.util.FileUtils;
 
 /**
  *	For Scraping repo commits for possible patch commits
@@ -58,7 +51,7 @@ import org.eclipse.jgit.util.FileUtils;
 public class PatchCommitScraper {
 
 	private static final Logger logger = LogManager.getLogger(PatchCommitScraper.class.getName());
-	private static final String[] patchRegex = new String[] {"vulnerability|Vulnerability|vuln|Vuln|VULN[ #]*([0-9]+)"};
+	private static final Pattern[] patchRegex = new Pattern[] {Pattern.compile("vulnerability|Vulnerability|vuln|Vuln|VULN[ #]*([0-9]+)")};
 	private final String localDownloadLoc;
 	private final String repoSource;
 
@@ -81,39 +74,43 @@ public class PatchCommitScraper {
 		logger.info("Grabbing Commits List for repo @ {}...", localDownloadLoc);
 
 		// Initialize commit list form the repo's .git folder
-		try (Repository repository = new FileRepositoryBuilder().setGitDir(new File(localDownloadLoc+"/.git")).build()){
-			// Iterate through each commit and check if there's a commit message that contains a CVE ID
-			// or the 'vulnerability' keyword
-			try (Git git = new Git(repository)) {
-				ObjectId startingRevision = repository.resolve("master");
-				// TODO: Fix this, startingRevision = null
-				Iterable<RevCommit> commits = git.log().add(startingRevision).call();
-				for (RevCommit commit : commits) {
-					// Check if the commit message matches any of the regex provided
-					for (String regex: patchRegex) {
-						Pattern pattern = Pattern.compile(regex);
-						Matcher matcher = pattern.matcher(commit.getFullMessage());
-						// If found the CVE ID is found, add the patch commit to the returned list
-						if (matcher.find() || commit.getFullMessage().contains(cveId)) {
-							String commitUrl = repository.getConfig().getString("remote", "origin", "url");
-							LocalDateTime commitDateTime = LocalDateTime.ofInstant(
-									Instant.ofEpochSecond(commit.getCommitTime()),
-									ZoneId.systemDefault()
-							);
-							logger.info("Found patch commit @ {} in repo {}", commitUrl, localDownloadLoc);
-							PatchCommit patchCommit = new PatchCommit(commitUrl, cveId, commit.getName(), commitDateTime, commit.getFullMessage());
-							patchCommits.add(patchCommit);
+		try (final Repository repository = new FileRepositoryBuilder().setGitDir(new File(localDownloadLoc+"/.git")).build()){
+			try(final Git git = new Git(repository)) {
+				// Iterate through each commit and check if there's a commit message that contains a CVE ID
+				// or the 'vulnerability' keyword
+				final ObjectId startingRevision = repository.resolve("refs/heads/master");
+				if(startingRevision != null) {
+					final Iterable<RevCommit> commits = git.log().add(startingRevision).call();
+
+					int ignoredCounter = 0;
+
+					for (RevCommit commit : commits) {
+						// Check if the commit message matches any of the regex provided
+						for (Pattern pattern : patchRegex) {
+							Matcher matcher = pattern.matcher(commit.getFullMessage());
+							// If found the CVE ID is found, add the patch commit to the returned list
+							if (matcher.find() || commit.getFullMessage().contains(cveId)) {
+								String commitUrl = repository.getConfig().getString("remote", "origin", "url");
+								LocalDateTime commitDateTime = LocalDateTime.ofInstant(
+										Instant.ofEpochSecond(commit.getCommitTime()),
+										ZoneId.systemDefault()
+								);
+								logger.info("Found patch commit @ {} in repo {}", commitUrl, localDownloadLoc);
+								PatchCommit patchCommit = new PatchCommit(commitUrl, cveId, commit.getName(), commitDateTime, commit.getFullMessage());
+								patchCommits.add(patchCommit);
+							} else ignoredCounter++;
 						}
 					}
-				}
 
-				if (patchCommits.isEmpty()) {
-					logger.info("No patches for CVE {} found in repo {} ", cveId, localDownloadLoc);
-				}
+					logger.info("Ignored {} non-patch commits", ignoredCounter);
+
+					if (patchCommits.isEmpty()) {
+						logger.info("No patches for CVE {} found in repo {} ", cveId, localDownloadLoc);
+					}
+				} else logger.warn("Could not get starting revision from repo {}", localDownloadLoc);
 			}
 		} catch (IOException | GitAPIException e) {
 			logger.error("ERROR: Failed to scrape repo @ {} for patch commits for CVE {}\n{}", repoSource, cveId, e);
-			e.printStackTrace();
 		}
 
 		return patchCommits;
