ad5321f939693cdb7d9e8f8df87411fd01848832
sns8777 <sns877@rit.edu>
2023-06-21 12:08:32 -0400

    all tests in DatabaseHelperTest are complete and pass

--- reconciler/src/test/java/edu/rit/se/nvip/db/DatabaseHelperTest.java
+++ reconciler/src/test/java/edu/rit/se/nvip/db/DatabaseHelperTest.java
@@ -24,7 +24,7 @@
 package edu.rit.se.nvip.db;
 
 import com.zaxxer.hikari.HikariDataSource;
-import model.*;
+import edu.rit.se.nvip.model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
