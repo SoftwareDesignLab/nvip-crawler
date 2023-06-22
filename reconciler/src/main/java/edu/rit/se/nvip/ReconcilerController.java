bcb18f7eedddfbbaf2ec21dfbd5cc0f68576aa42
memeeerit <memeee@rit.edu>
2023-06-22 13:23:05 -0400

    reconciler reforms compvuln if old version is low prio and new sources are high prio

--- reconciler/src/main/java/edu/rit/se/nvip/ReconcilerController.java
+++ reconciler/src/main/java/edu/rit/se/nvip/ReconcilerController.java
@@ -4,6 +4,7 @@ import edu.rit.se.nvip.filter.Filter;
 import edu.rit.se.nvip.filter.FilterFactory;
 import edu.rit.se.nvip.model.CompositeVulnerability;
 import edu.rit.se.nvip.model.RawVulnerability;
+import edu.rit.se.nvip.model.VulnSetWrapper;
 import edu.rit.se.nvip.process.Processor;
 import edu.rit.se.nvip.process.ProcessorFactory;
 import edu.rit.se.nvip.reconciler.Reconciler;
@@ -75,7 +76,7 @@ public class ReconcilerController {
         logger.info("{} raw vulnerabilities with CVE ID {} were found and {} were new.\n" +
                         "The first wave of filtering passed {} out of {} new high priority sources.\n" +
                         "The second wave of filtering passed {} out of {} new backup low priority sources.\n" +
-                        "In total, {} distinct descriptions had to be filtered.",
+                        "In total, {} distinct descriptions were explicitly filtered.",
                 rawCount, cveId, newRawCount,
                 firstWaveReturn.numPassed, firstWaveReturn.numIn,
                 secondWaveReturn.numPassed, secondWaveReturn.numIn,
@@ -138,51 +139,4 @@ public class ReconcilerController {
         filters.add(FilterFactory.createFilter(FilterFactory.DESCRIPTION_SIZE));
     }
 
-    private static class VulnSetWrapper  {
-        private final Set<RawVulnerability> vulns;
-        public VulnSetWrapper(Set<RawVulnerability> vulns) {
-            this.vulns = vulns;
-        }
-        public boolean hasPassedHighPrio() {
-            for (RawVulnerability v : vulns) {
-                if (v.getFilterStatus() == RawVulnerability.FilterStatus.PASSED && v.isHighPriority()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        public Set<RawVulnerability> firstFilterWave() {
-            // first wave of filtering will be any high priority sources that haven't been evaluated yet
-            return vulns.stream().filter(v->!v.isFiltered()).filter(RawVulnerability::isHighPriority).collect(Collectors.toSet());
-        }
-        public Set<RawVulnerability> secondFilterWave() {
-            if (hasPassedHighPrio()) {
-                // no need for a second wave
-                return new HashSet<>();
-            }
-            // all the high prio sources failed, release the second wave of low prio sources
-            return vulns.stream().filter(v->!v.isFiltered()).collect(Collectors.toSet());
-        }
-
-        public int setNewToUneval() {
-            int out = 0;
-            for (RawVulnerability v : vulns) {
-                if (v.getFilterStatus() == RawVulnerability.FilterStatus.NEW) {
-                    out ++;
-                    v.setFilterStatus(RawVulnerability.FilterStatus.UNEVALUATED);
-                }
-            }
-            return out;
-        }
-
-        public Set<RawVulnerability> toUpdate() {
-            return vulns.stream().filter(RawVulnerability::filterStatusChanged).collect(Collectors.toSet());
-        }
-        public Set<RawVulnerability> toReconcile() {
-            // if a vuln was changed, it was unevaluated before and thus hasn't been considered for prior reconciliation
-            // then take the changed ones and pick out the passed vulns
-            // don't need to worry about prio here because nothing gets filtered that we don't want to use
-            return toUpdate().stream().filter(v->v.getFilterStatus()== RawVulnerability.FilterStatus.PASSED).collect(Collectors.toSet());
-        }
-    }
 }
