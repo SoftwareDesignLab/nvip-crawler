5e36fd82c967a461664d1acf99bbeec8421c0ad2
Andromeda Sawtelle <zts7211@rit.edu>
2023-06-28 10:56:29 -0400

    Changed RedHatParser to follow UTC instead of the local timezone

--- crawler/src/test/java/edu/rit/se/nvip/crawler/htmlparser/RedHatParserTest.java
+++ crawler/src/test/java/edu/rit/se/nvip/crawler/htmlparser/RedHatParserTest.java
@@ -67,7 +67,7 @@ public class RedHatParserTest extends AbstractParserTest {
 		assertEquals("CVE-2023-25725", sample.getCveId());
 		assertTrue(sample.getDescription().contains("A flaw was found in HAProxy's headers processing that causes HAProxy to drop important headers fields such as Connection, Content-length, Transfer-Encoding,"));
 		assertEquals("2023-02-14 16:20:00", sample.getPublishDate());
-		assertEquals("2023-06-24 06:06:14", sample.getLastModifiedDate());
+		assertEquals("2023-06-24 10:06:14", sample.getLastModifiedDate());
 
 	}
 }
