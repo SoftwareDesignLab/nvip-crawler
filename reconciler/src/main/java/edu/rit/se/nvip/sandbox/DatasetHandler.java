5d88ede70652fa77497ce21f87407ed0dcb4e52a
jqm4954 <jqm4954@rit.edu>
2023-06-28 15:33:27 -0400

    Updating DatasetHandler with methods for CVE analysis

--- reconciler/src/main/java/edu/rit/se/nvip/sandbox/DatasetHandler.java
+++ reconciler/src/main/java/edu/rit/se/nvip/sandbox/DatasetHandler.java
@@ -23,8 +23,9 @@ public class DatasetHandler {
         DatasetHandler dh = new DatasetHandler();
 //        dh.jsonToDb();
 //        dh.dbToJson();
-//        dh.updateJson("./src/main/java/edu/rit/se/nvip/sandbox/CrawlerOutputFull_6_22_2023.json");
+        dh.updateJson("./src/main/java/edu/rit/se/nvip/sandbox/OUTDATEDFORMAT_CrawlerOutputFull_6_22_2023.json");
 //        dh.firstSecondWaveFilterMetrics("./src/main/java/edu/rit/se/nvip/sandbox/GenericParserOutputFull_6_22_2023.json");
+//        dh.analyzeCveForMatt();
     }
 
     public DatasetHandler() {
@@ -180,9 +181,9 @@ public class DatasetHandler {
             ob.add("raw_description_id", jo.getInt("raw_description_id"));
             ob.add("raw_description", jo.getString("raw_description"));
             ob.add("cve_id", jo.getString("cve_id"));
-            ob.add("created_date", jo.getInt("created_date"));
-            ob.add("published_date", jo.getInt("published_date"));
-            ob.add("last_modified_date", jo.getInt("last_modified_date"));
+            ob.add("created_date", jo.getJsonNumber("created_date").longValue());
+            ob.add("published_date", jo.getJsonNumber("published_date").longValue());
+            ob.add("last_modified_date", jo.getJsonNumber("last_modified_date").longValue());
             ob.add("source_url", jo.getString("source_url"));
             ob.add("source_type", currentSourceType);
             ob.add("filter_status", 1);
@@ -190,7 +191,7 @@ public class DatasetHandler {
         }
         JsonArray ja = builder.build();
 
-        try (FileWriter writer = new FileWriter(jsonPath)) {
+        try (FileWriter writer = new FileWriter("./src/main/java/edu/rit/se/nvip/sandbox/CrawlerOutputFull_6_22_2023.json")) {
             writer.write(ja.toString());
         } catch (IOException e) {
             e.printStackTrace();
@@ -230,8 +231,8 @@ public class DatasetHandler {
 
         //Mimic reconciler controller process
         VulnSetWrapper wrapper = new VulnSetWrapper(rawVulns);
+
         Set<RawVulnerability> firstWaveVulns = wrapper.firstFilterWave();
-        Set<RawVulnerability> secondWaveVulns = wrapper.secondFilterWave();
         //Calculate metrics for first wave
         Map<String, Set<RawVulnerability>> equivClasses = new HashMap<>();
         Set<RawVulnerability> samples = new HashSet<>(); // holds one from each equivalence class
@@ -258,6 +259,7 @@ public class DatasetHandler {
         System.out.println("Accepted: " + numPassed);
 
         //Calculate metrics for second
+        Set<RawVulnerability> secondWaveVulns = wrapper.secondFilterWave();
         Map<String, Set<RawVulnerability>> equivClasses2 = new HashMap<>();
         Set<RawVulnerability> samples2 = new HashSet<>(); // holds one from each equivalence class
         for (RawVulnerability rawVuln : secondWaveVulns) {
@@ -279,7 +281,132 @@ public class DatasetHandler {
         }
         int numPassed2 = secondWaveVulns.stream().filter(v->v.getFilterStatus() == RawVulnerability.FilterStatus.PASSED).collect(Collectors.toSet()).size();
         System.out.println("Total in json: " + jArray.size());
-        System.out.println("Total in first wave: " + secondWaveVulns.size());
+        System.out.println("Total in second wave: " + secondWaveVulns.size());
         System.out.println("Accepted: " + numPassed2);
+    }
+
+    public void analyzeCveForMatt() {
+        String jsonPathFrom = "./src/main/java/edu/rit/se/nvip/sandbox/CrawlerOutputFull_6_22_2023.json";
+//        String jsonPathTo = "./src/main/java/edu/rit/se/nvip/sandbox/CrawlerOutputFull_6_22_2023_NEW.json";
+        LinkedHashMap<String, Integer> cveCounts = new LinkedHashMap<>();
+        LinkedHashMap<String, String> cveSources = new LinkedHashMap<>();
+        JsonArray jArray = null;
+        try (FileReader reader = new FileReader(jsonPathFrom)) {
+            JsonReader jReader = Json.createReader(reader);
+            jArray = jReader.readArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        for (int i = 0; i < jArray.size(); i++) {
+            JsonObject jo = jArray.getJsonObject(i);
+            String cveId = jo.getString("cve_id");
+            String cveSource = jo.getString("source_url");
+            String host = "";
+            try {
+                URI url = new URI(cveSource);
+                host = url.getHost();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+            if (cveCounts.containsKey(cveId)) {
+//              Only increment if different domains
+//                if (!cveSources.containsValue(host)) {
+                    int currentCount = cveCounts.get(cveId);
+                    cveCounts.put(cveId, currentCount+1);
+                    cveSources.put(cveId, host);
+//                }
+            } else {
+                cveCounts.put(cveId, 1);
+                cveSources.put(cveId, host);
+            }
+        }
+        for (String currentCveId: cveCounts.keySet()) {
+            if (cveCounts.get(currentCveId) >=4) {
+                createJson(jsonPathFrom, currentCveId);
+            }
+        }
+    }
+
+    private void createJson(String jsonPathFrom, String cveId) {
+        JsonArray jArray = null;
+        try (FileReader reader = new FileReader(jsonPathFrom)) {
+            JsonReader jReader = Json.createReader(reader);
+            jArray = jReader.readArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        JsonArrayBuilder builder = Json.createArrayBuilder();
+        Set<RawVulnerability> rawVulns = new HashSet<>();
+        for (int i = 0; i < jArray.size(); i++) {
+            JsonObject jo = jArray.getJsonObject(i);
+            if (jo.getString("cve_id").equals(cveId)) {
+                RawVulnerability rawVuln = new RawVulnerability(jo.getInt("raw_description_id"),
+                        jo.getString("cve_id"),
+                        jo.getString("raw_description"),
+                        new Timestamp(jo.getJsonNumber("published_date").longValue()),
+                        new Timestamp(jo.getJsonNumber("last_modified_date").longValue()),
+                        new Timestamp(jo.getJsonNumber("created_date").longValue()),
+                        jo.getString("source_url"),
+                        jo.getString("source_type"),
+                        jo.getInt("filter_status"));
+                rawVulns.add(rawVuln);
+            }
+        }
+        Set<Filter> filters = new HashSet<>();
+        filters.add(FilterFactory.createFilter(FilterFactory.MULTIPLE_CVE_DESCRIPTION));
+        filters.add(FilterFactory.createFilter(FilterFactory.BLANK_DESCRIPTION));
+        filters.add(FilterFactory.createFilter(FilterFactory.INTEGER_DESCRIPTION));
+        filters.add(FilterFactory.createFilter(FilterFactory.DESCRIPTION_SIZE));
+        filters.add(FilterFactory.createFilter(FilterFactory.CVE_MATCHES_DESCRIPTION));
+
+        //Run tests on collected set of vulns
+        int numHighPrio = 0;
+        int numLowPrio = 0;
+        for (RawVulnerability rawVuln: rawVulns) {
+            if (rawVuln.isHighPriority())
+                numHighPrio++;
+            else
+                numLowPrio++;
+        }
+
+        //Analyze descriptions
+        int numPassFilter = 0;
+        int numFailFilter = 0;
+        for (RawVulnerability rawVuln: rawVulns) {
+            for (Filter filter: filters) {
+                if (!filter.passesFilter(rawVuln)) {
+                    numFailFilter++;
+                    break;
+                }
+            }
+            numPassFilter++;
+        }
+
+        if (numHighPrio != 0 && numLowPrio != 0 && numFailFilter != 0 && numPassFilter != 0) {
+            for (RawVulnerability rawVuln: rawVulns) {
+                JsonObjectBuilder job = Json.createObjectBuilder();
+                job.add("raw_description_id", rawVuln.getId());
+                job.add("raw_description", rawVuln.getDescription());
+                job.add("cve_id", rawVuln.getCveId());
+                job.add("created_date", rawVuln.getCreateDate().toString());
+                job.add("published_date", rawVuln.getPublishDate().toString());
+                job.add("last_modified_date", rawVuln.getLastModifiedDate().toString());
+                job.add("source_url", rawVuln.getSourceUrl());
+                job.add("source_type", rawVuln.getSourceType().type);
+                job.add("filter_status", rawVuln.getFilterStatus().value);
+                builder.add(job);
+            }
+            JsonArray ja = builder.build();
+
+            try (FileWriter writer = new FileWriter("./src/main/java/edu/rit/se/nvip/sandbox/jsons/" + cveId + ".json")) {
+                writer.write(ja.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
