3f2e6d62ea64b5139fc61c9c7c501c45572d61df
sns8777 <sns877@rit.edu>
2023-06-28 17:54:06 -0400

    made constructor and gettors for CrawlerRun and made newVulnsPerRun for FilterMetrics, FitlerMetrics constructor still work in progress

--- reconciler/src/main/java/edu/rit/se/nvip/utils/metrics/CrawlerRun.java
+++ reconciler/src/main/java/edu/rit/se/nvip/utils/metrics/CrawlerRun.java
@@ -3,16 +3,23 @@ package edu.rit.se.nvip.utils.metrics;
 import edu.rit.se.nvip.model.RawVulnerability;
 
 import java.util.Date;
-import java.util.Map;
 import java.util.Set;
 
 public class CrawlerRun {
     private Set<RawVulnerability> vulns;
     private int runId;
     private Date runDate;
-    public CrawlerRun(Set<RawVulnerability> vulns, int runId, Date date) {
-        return;
+    public CrawlerRun(Set<RawVulnerability> vulns, int runId, Date runDate) {
+        this.vulns = vulns;
+        this.runId = runId;
+        this.runDate = runDate;
     }
+
+    public int getRunId(){return this.runId;}
+
+    public Date getRunDate(){return this.runDate;}
+
+    public Set<RawVulnerability> getVulns(){return this.vulns;}
 
     // todo maybe some FilterMetrics methods should use CrawlerRun methods ;)
 }
