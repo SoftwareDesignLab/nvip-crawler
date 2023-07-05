2735d9f345c729225103614381a1bfe64f7431c1
ras7019 <46506471+q1king@users.noreply.github.com>
2023-07-05 15:51:35 -0400

    testparsecommitobjects is up but it needs some more work

--- patchfinder/src/test/java/PatchFinderThreadTest.java
+++ patchfinder/src/test/java/PatchFinderThreadTest.java
@@ -2,10 +2,17 @@ import commits.PatchCommit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.helger.commons.mock.CommonsAssert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -50,4 +57,22 @@ public class PatchFinderThreadTest {
         assertEquals(24, patchCommits.size());
 
     }
+
+    @Test
+    public void testParseCommitObjects() throws IOException {
+        HashMap<String, ArrayList<String>> cvePatchEntry = new HashMap<>();
+        ArrayList<String> patchSources = new ArrayList<>();
+        patchSources.add("https://github.com/pjsip/pjproject");
+        cvePatchEntry.put("CVE-2022-31031", patchSources);
+//        String clonePath = PatchFinder.clonePath;
+//        long timeoutMilli = 5000;
+
+        //want parseCommitObjects to be called, so we have to check the url using findPatchCommitsFromUrl
+        PatchFinder.cloneCommitThreshold = 7000;
+        PatchFinder.findPatchesMultiThreaded(cvePatchEntry);
+        List<PatchCommit> patchCommits = PatchFinder.getPatchCommits();
+        assertEquals(24, patchCommits.size());
+
+    }
+
 }
