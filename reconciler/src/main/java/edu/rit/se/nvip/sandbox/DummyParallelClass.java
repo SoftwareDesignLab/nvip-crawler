40967fda067f0e22203eb96ac9de6055af767376
sns8777 <sns877@rit.edu>
2023-07-11 15:15:11 -0400

    proof of concept done

--- reconciler/src/main/java/edu/rit/se/nvip/sandbox/DummyParallelClass.java
+++ reconciler/src/main/java/edu/rit/se/nvip/sandbox/DummyParallelClass.java
@@ -2,18 +2,8 @@ package edu.rit.se.nvip.sandbox;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class DummyParallelClass {
-    private BlockingQueue<Integer> queue1;
-    private BlockingQueue<Integer> queue2;
-    private final Object key = new Object();
-
-    public DummyParallelClass() {
-        this.queue1 = new LinkedBlockingQueue<>();
-        this.queue2 = new LinkedBlockingQueue<>();
-    }
 
     public static void main(String[] args) {
         DummyParallelClass dummyParallel = new DummyParallelClass();
@@ -30,45 +20,37 @@ public class DummyParallelClass {
     public void runJobsInHalfParallel(Set<Integer> jobs) {
         for (Integer job : jobs) { //for each job
             Thread thread1 = new Thread(() -> { //thread that does step 1
-                synchronized (key) {
-                    step1(job);
-                }
+                step1(job);
             });
 
             Thread thread2 = new Thread(() -> { //thread that does step 2
-                synchronized (key) {
-                    step2(job);
-                }
+                step2(job);
             });
 
             thread1.start(); //do step 1
 
             try {
-                thread1.join(); //wait for step 1 to complete
+                thread1.join(); //wait for step 1 to end
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            thread2.start(); //start step 2
+            thread2.start(); //do step 2
 
-            try {
-                thread2.join(); //wait for step 2 to complete
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
-    public void step1(int job) { //filtering
+    public void step1(int job) { //mock filtering
         System.out.println("step 1 started for job " + job);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
     }
 
-    public void step2(int job) { //reconciling
+    public void step2(int job) { //mock reconciling
         System.out.println("step 2 started for job " + job);
         try {
             Thread.sleep(1000);
