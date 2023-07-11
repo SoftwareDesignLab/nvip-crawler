c4e92a5d1519177b21bec952d57ffb5b35f1ba23
sns8777 <sns877@rit.edu>
2023-07-11 13:59:52 -0400

    close to completion on proof of concept

--- reconciler/src/main/java/edu/rit/se/nvip/sandbox/DummyParallelClass.java
+++ reconciler/src/main/java/edu/rit/se/nvip/sandbox/DummyParallelClass.java
@@ -1,4 +1,5 @@
 package edu.rit.se.nvip.sandbox;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
@@ -7,6 +8,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class DummyParallelClass {
     private BlockingQueue<Integer> queue1;
     private BlockingQueue<Integer> queue2;
+    private final Object key = new Object();
 
     public DummyParallelClass() {
         this.queue1 = new LinkedBlockingQueue<>();
@@ -23,61 +25,56 @@ public class DummyParallelClass {
         jobs.add(3);
 
         dummyParallel.runJobsInHalfParallel(jobs);
-
-        // Run the jobs in a half-parallel manner
-//        for (int i = 5; i > 0; i--){
-//            dummyParallel.runJobsInHalfParallel(jobs);
-//        }
     }
 
     public void runJobsInHalfParallel(Set<Integer> jobs) {
-        for (Integer job : jobs) {
-            Thread thread1 = new Thread(() -> step1(job));
-            Thread thread2 = new Thread(() -> step2(job));
+        for (Integer job : jobs) { //for each job
+            Thread thread1 = new Thread(() -> { //thread that does step 1
+                synchronized (key) {
+                    step1(job);
+                }
+            });
+
+            Thread thread2 = new Thread(() -> { //thread that does step 2
+                synchronized (key) {
+                    step2(job);
+                }
+            });
+
+            thread1.start(); //do step 1
 
             try {
-                // Start thread1 first
-                thread1.start();
-
-                // Wait for thread1 to populate queue1 before starting thread2
-                thread1.join();
-
-                // Start thread2 after thread1 has populated queue1
-                thread2.start();
-
-                // Wait for both threads to complete
-                thread2.join();
+                thread1.join(); //wait for step 1 to complete
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
+            thread2.start(); //start step 2
+
+            try {
+                thread2.join(); //wait for step 2 to complete
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
 
-    public void step1(int job) {
+    public void step1(int job) { //filtering
+        System.out.println("step 1 started for job " + job);
         try {
-            // Perform step1 operation and put the result into queue1
-            int result = job + 1;
-            queue1.put(result);
-            System.out.println("job finished");
             Thread.sleep(1000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
-    public void step2(int job) {
+    public void step2(int job) { //reconciling
+        System.out.println("step 2 started for job " + job);
         try {
-            // Pull from queue1 and perform step2 operation
-            int data = queue1.take();
-            int result = data + 2;
-
-            // Put the result into queue2
-            queue2.put(result);
-
-            // Finish the job
-            System.out.println("Job " + job + " completed. Result: " + result);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        System.out.println("step 2 finished for job " + job);
     }
 }
