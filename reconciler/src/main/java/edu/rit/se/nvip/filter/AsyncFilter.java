5ff0f367185013bb0c804ecb000ffaf133f2ff62
memeeerit <memeee@rit.edu>
2023-07-13 13:51:38 -0400

    async filter timeout adjustment

--- reconciler/src/main/java/edu/rit/se/nvip/filter/AsyncFilter.java
+++ reconciler/src/main/java/edu/rit/se/nvip/filter/AsyncFilter.java
@@ -20,11 +20,11 @@ public abstract class AsyncFilter extends Filter {
             executor.submit(new FilterTask(vuln));
         }
         try {
-            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            executor.shutdown();
+            executor.awaitTermination(rawVulns.size() * 60L, TimeUnit.SECONDS); // wait up to a minute per vuln. todo be smarter about this.
         } catch (InterruptedException ex) {
             logger.error(ex);
         }
-        executor.shutdown();
     }
 
     private class FilterTask implements Runnable {
