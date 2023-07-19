17fbfc2e3409d0d11e7c7050bf033c015688ecb5
Richard Sawh <ras7019@g.rit.edu>
2023-07-19 10:18:51 -0400

    fixed gitcontroller main test and increased consistency

--- patchfinder/src/test/java/utils/GitControllerTest.java
+++ patchfinder/src/test/java/utils/GitControllerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class GitControllerTest {
-    private static final String LOCAL_PATH = "testrepo";
+    private static final String LOCAL_PATH = "resources/patch-repos";
 
     private static final String REMOTE_PATH = "https://github.com/apache/airflow.git";
     private GitController gitController;
@@ -32,14 +32,14 @@ public class GitControllerTest {
 
     @Test
     public void testMain_DeleteRepo() {
-        // Create a mock GitController
-        GitController gitMock = mock(GitController.class);
-
         // Invoke the method under test
         GitController.main(new String[]{});
 
         // Verify the behavior and result
         assertTrue(Files.exists(Paths.get(LOCAL_PATH)));
+
+        //rebuild the repo
+        gitController.cloneRepo();
     }
 
 }
