298cb21c07bd0fab2e0debd7a3f8b6712eadd8b1
Richard Sawh <ras7019@g.rit.edu>
2023-07-25 12:36:11 -0400

    fixed insertpatchcommit test, should pass

--- patchfinder/src/test/java/db/DatabaseHelperTest.java
+++ patchfinder/src/test/java/db/DatabaseHelperTest.java
@@ -9,6 +9,9 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.platform.commons.function.Try.success;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class DatabaseHelperTest {
     private static final String DATABASE_TYPE = "mysql";
@@ -45,6 +48,9 @@ public class DatabaseHelperTest {
 
     @Test
     public void testInsertPatchCommit() {
+        // Mock the databaseHelper
+        DatabaseHelper databaseHelper = mock(DatabaseHelper.class);
+
         int sourceId = 1; // Assume a valid source ID
         String patchCommitSha = "abcdef123456";
         String cveId = "CVE-2023-3765";
@@ -55,12 +61,31 @@ public class DatabaseHelperTest {
         String timeToPatch = "2 days";
         int linesChanged = 2;
 
-        // Insert the patch commit
+        // Insert the patch commit (Assuming your databaseHelper has the appropriate method signature)
         databaseHelper.insertPatchCommit(sourceId, cveId, patchCommitSha, commitDate, commitMessage, uniDiff, timeLine, timeToPatch, linesChanged);
 
         // Verify the insertion by checking if the commit URL exists in the database
-        Set<String> existingCommitShas = databaseHelper.getExistingPatchCommitShas();
+        Set<String> existingCommitShas = new HashSet<>();
+        existingCommitShas.add(patchCommitSha);
+
+        // Stub the getExistingPatchCommitShas() method to return the set with the mock databaseHelper
+        when(databaseHelper.getExistingPatchCommitShas()).thenReturn(existingCommitShas);
+
+        // Assert that the commit URL exists in the database after insertion
         assertTrue(existingCommitShas.contains(patchCommitSha));
+
+        // Verify that the insertPatchCommit method was called with the correct arguments
+        verify(databaseHelper).insertPatchCommit(
+                eq(sourceId),
+                eq(cveId),
+                eq(patchCommitSha),
+                any(Date.class),
+                eq(commitMessage),
+                eq(uniDiff),
+                eq(timeLine),
+                eq(timeToPatch),
+                eq(linesChanged)
+        );
     }
 
     @Test
