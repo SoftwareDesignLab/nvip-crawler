4e377922c79478216bc33fe3197d2a076466bb82
Richard Sawh <ras7019@g.rit.edu>
2023-08-22 11:54:14 -0400

    db updated with fix functionality

--- patchfinder/src/main/java/db/DatabaseHelper.java
+++ patchfinder/src/main/java/db/DatabaseHelper.java
@@ -57,6 +57,9 @@ public class DatabaseHelper {
 	private final String deletePatchCommitSql = "DELETE FROM patchcommit WHERE commit_sha = ?;";
 	private final String getCveSourcesSql = "SELECT cve_id, source_url FROM nvip.rawdescription WHERE source_url != \"\";";
 	private final String getCveSourcesNVDSql = "SELECT cve_id, source_url FROM nvip.nvdsourceurl;";
+	private final String insertFixSourceURLSql = "INSERT INTO fixes (cve_id, source_url) VALUES (?, ?);";
+	private final String getExistingFixSourceUrlsSql = "SELECT source_url FROM fixes;";
+	private final String insertFixSql = "INSERT INTO fixes (fix_id, cve_id, fix_description, source_url_id) VALUES (?, ?, ?, ?);";
 	public static final Pattern CPE_PATTERN = Pattern.compile("cpe:2\\.3:[aho\\*\\-]:([^:]*):([^:]*):([^:]*):.*");
 
 	/**
@@ -389,7 +392,6 @@ public class DatabaseHelper {
 		final Map<String, Integer> urls = new HashMap<>();
 
 		try (Connection connection = getConnection();
-			 // TODO: Implement SQL statement
 			 PreparedStatement pstmt = connection.prepareStatement(getExistingFixSourceUrlsSql)) {
 			ResultSet rs = pstmt.executeQuery();
 			while(rs.next()) { urls.put(rs.getString(1), rs.getInt(2)); }
@@ -536,7 +538,6 @@ public class DatabaseHelper {
 			// Get and return id from map
 			return existingSourceUrls.get(sourceURL);
 		} else { // Otherwise, insert and return generated id
-			// TODO: Implement SQL statement
 			try (Connection conn = getConnection(); PreparedStatement pstmt = conn.prepareStatement(insertFixSourceURLSql, Statement.RETURN_GENERATED_KEYS)) {
 				pstmt.setString(1, cve_id);
 				pstmt.setString(2, sourceURL);
@@ -562,64 +563,43 @@ public class DatabaseHelper {
 	// TODO: Implement
 	/**
 	 * Method for inserting a fix into the fixes table
-	 *
-	 * @param sourceId id of the source url
-	 * @param commitSha commit SHA
-	 * @param commitDate commit date
-	 * @param commitMessage commit message
-	 * @param uniDiff unified diff String
-	 * @param timeLine timeline list of String objects
-	 * @param timeToPatch time from CVE release -> patch release
-	 * @param linesChanged number of lines changed
+	 * Should also check for duplicates
+	 * @param fix_id id of the fix
+	 * @param cve_id CVE being processed
+	 * @param fix_description description of the fix
+	 * @param source_url_id id of the source url
 	 * @throws IllegalArgumentException if given source id is invalid (sourceId < 0)
 	 */
-	public void insertFix(int sourceId, String cveId, String commitSha, java.util.Date commitDate, String commitMessage, String uniDiff, List<String> timeLine, String timeToPatch, int linesChanged) throws IllegalArgumentException {
-//		if (sourceId < 0) throw new IllegalArgumentException("Invalid source id provided, ensure id is non-negative");
-//
-//		try (Connection connection = getConnection();
-//			 PreparedStatement pstmt = connection.prepareStatement(insertPatchCommitSql);
-//			 PreparedStatement pstmtExistingCommit = connection.prepareStatement("SELECT commit_sha FROM patchcommit WHERE commit_sha = ? LIMIT 1");
-//			 PreparedStatement pstmtUpdateCommit = connection.prepareStatement("UPDATE patchcommit SET commit_date = ?, commit_message = ?, uni_diff = ?, timeline = ?, time_to_patch = ?, lines_changed = ? WHERE commit_sha = ?")
-//		) {
-//			// Check if the commit URL already exists in the database
-//			pstmtExistingCommit.setString(1, commitSha);
-//			ResultSet existingCommitResult = pstmtExistingCommit.executeQuery();
-//
-//			if (existingCommitResult.next()) {
-//				// Existing commit found
-//				logger.warn("Patch commit '{}' already exists in the database", commitSha);
-//
-//				// Perform the appropriate action for existing entries (diff, replace, ignore)
-//				// Here, we are updating the existing commit with the new information
-//				pstmtUpdateCommit.setDate(1, new java.sql.Date(commitDate.getTime()));
-//				pstmtUpdateCommit.setString(2, commitMessage);// TODO: Fix data truncation error
-//				pstmtUpdateCommit.setString(3, uniDiff);
-//				pstmtUpdateCommit.setString(4, timeLine.toString());
-//				pstmtUpdateCommit.setString(5, timeToPatch);
-//				pstmtUpdateCommit.setInt(6, linesChanged);
-//				pstmtUpdateCommit.setString(7, commitSha);
-//				pstmtUpdateCommit.executeUpdate();
-//
-//				logger.info("Existing patch commit updated: {}", commitSha);
-//			} else {
-//				// Insert the new patch commit
-//				pstmt.setInt(1, sourceId);
-//				pstmt.setString(2, cveId);
-//				pstmt.setString(3, commitSha);
-//				pstmt.setDate(4, new java.sql.Date(commitDate.getTime()));
-//				pstmt.setString(5, commitMessage);
-//				pstmt.setString(6, uniDiff);
-//				pstmt.setString(7, timeLine.toString());
-//				pstmt.setString(8, timeToPatch);
-//				pstmt.setInt(9, linesChanged);
-//				pstmt.executeUpdate();
-//
-//				logger.info("New patch commit inserted: {}", commitSha);
-//			}
-//		} catch (Exception e) {
-//			logger.error("ERROR: Failed to insert/update patch commit from source {}: {}", commitSha, e);
-//			throw new IllegalArgumentException(e);
-//		}
+	public void insertFix(int fix_id, int cve_id, String fix_description, int source_url_id) throws IllegalArgumentException, SQLException {
+		if (source_url_id < 0) throw new IllegalArgumentException("Invalid source id provided, ensure id is non-negative");
+
+		try (Connection connection = getConnection();
+			 PreparedStatement pstmt = connection.prepareStatement(insertFixSql);
+			 PreparedStatement pstmtExistingCommit = connection.prepareStatement("SELECT fix_id FROM fixes WHERE fix_id = ? LIMIT 1");
+			 PreparedStatement pstmtUpdateFix = connection.prepareStatement("SELECT fix_id FROM fixes WHERE fix_id = ? AND fix_description = ? LIMIT 1");
+		) {
+			// Check if the fix already exists
+			pstmtExistingCommit.setInt(1, cve_id);
+			ResultSet rs = pstmtExistingCommit.executeQuery();
+			if (rs.next()) {
+				logger.info("Fix already exists for CVE ID {}", cve_id);
+				//updateFix(fix_id, cve_id, fix_description, source_url_id);
+				pstmtUpdateFix.setInt(1, fix_id);
+				pstmtUpdateFix.setString(2, fix_description);
+				rs = pstmtUpdateFix.executeQuery();
+			} else {
+				// Insert the fix
+				pstmt.setInt(1, fix_id);
+				pstmt.setInt(2, cve_id);
+				pstmt.setString(3, fix_description);
+				pstmt.setInt(4, source_url_id);
+				pstmt.executeUpdate();
+				logger.info("Inserted fix for CVE ID {}", cve_id);
+			}
+		}
+
+
+
 	}
 
 	/**
