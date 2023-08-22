1af732a19d2835ac61c32ca63daa239dd3b2475e
Owner <sns8777@rit.edu>
2023-08-22 09:26:48 -0400

    all tests are above 80% coverage, openAI tests are not covered but are also not used

---
+++ reconciler/src/test/java/edu/rit/se/nvip/ReconcilerControllerTest.java
@@ -0,0 +1,80 @@
+package edu.rit.se.nvip;
+
+import edu.rit.se.nvip.characterizer.CveCharacterizer;
+import edu.rit.se.nvip.filter.FilterHandler;
+import edu.rit.se.nvip.filter.FilterReturn;
+import edu.rit.se.nvip.messenger.Messenger;
+import edu.rit.se.nvip.mitre.MitreCveController;
+import edu.rit.se.nvip.model.CompositeVulnerability;
+import edu.rit.se.nvip.model.RawVulnerability;
+import edu.rit.se.nvip.model.RunStats;
+import edu.rit.se.nvip.nvd.NvdCveController;
+import edu.rit.se.nvip.reconciler.Reconciler;
+import edu.rit.se.nvip.utils.ReconcilerEnvVars;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ReconcilerControllerTest {
+
+    @Test
+    void mainTest() {
+        ReconcilerController rc = new ReconcilerController();
+        MockedStatic<ReconcilerEnvVars> mockedEnvVars = mockStatic(ReconcilerEnvVars.class);
+        DatabaseHelper mockDbh = mock(DatabaseHelper.class);
+        FilterHandler mockFH = mock(FilterHandler.class);
+        Reconciler mockRecon = mock(Reconciler.class);
+        FilterReturn mockFR = mock(FilterReturn.class);
+        Messenger mockMes = mock(Messenger.class);
+        MitreCveController mockMitre = mock(MitreCveController.class);
+        NvdCveController mockNvd = mock(NvdCveController.class);
+        CveCharacterizer mockChar = mock(CveCharacterizer.class);
+        rc.setDbh(mockDbh);
+        rc.setReconciler(mockRecon);
+        rc.setFilterHandler(mockFH);
+        rc.setMessenger(mockMes);
+        rc.setNvdController(mockNvd);
+        rc.setMitreController(mockMitre);
+        rc.setCveCharacterizer(mockChar);
+
+        mockedEnvVars.when(ReconcilerEnvVars::getDoCharacterization).thenReturn(true);
+        Set<RawVulnerability> rawVulns = new HashSet<>();
+        RawVulnerability raw = new RawVulnerability(1, "", "description1", null, null, null, "");
+        RawVulnerability raw1 = new RawVulnerability(2, "", "description2", null, null, null, "");
+        RawVulnerability raw2 = new RawVulnerability(3, "", "description3", null, null, null, "");
+        rawVulns.add(raw);
+        rawVulns.add(raw1);
+        rawVulns.add(raw2);
+        CompositeVulnerability vuln = new CompositeVulnerability(raw);
+        when(mockDbh.getRawVulnerabilities(anyString())).thenReturn(rawVulns);
+        when(mockDbh.getCompositeVulnerability(anyString())).thenReturn(vuln);
+        when(mockFH.runFilters(anySet())).thenReturn(mockFR);
+        doNothing().when(mockDbh).updateFilterStatus(anySet());
+        when(mockRecon.reconcile(any(CompositeVulnerability.class), anySet())).thenReturn(vuln);
+        when(mockDbh.insertOrUpdateVulnerabilityFull(any(CompositeVulnerability.class))).thenReturn(1);
+        doNothing().when(mockMes).sendPNEMessage(anyList());
+        when(mockDbh.insertTimeGapsForNewVulns(anySet())).thenReturn(1);
+        when(mockDbh.insertRun(any(RunStats.class))).thenReturn(1);
+        when(mockDbh.insertCvssBatch(anySet())).thenReturn(1);
+        when(mockDbh.insertVdoBatch(anySet())).thenReturn(1);
+        doNothing().when(mockMitre).updateMitreTables(anyBoolean());
+        doNothing().when(mockNvd).updateNvdTables(anyBoolean());
+        when(mockChar.characterizeCveList(anyList(), anyInt())).thenReturn(new ArrayList<>());
+
+
+        Set<String> jobs = new HashSet<>();
+        jobs.add("CVE-2023-1");
+        jobs.add("CVE-2023-2");
+        jobs.add("CVE-2023-3");
+        jobs.add("CVE-2023-4");
+        rc.main(jobs);
+
+        mockedEnvVars.close();
+    }
+}
