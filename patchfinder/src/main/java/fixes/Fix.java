07ecd019dd159b0d457d6588415a8642e523fa86
Richard Sawh <ras7019@g.rit.edu>
2023-08-23 11:50:25 -0400

    fix model created

--- patchfinder/src/main/java/fixes/Fix.java
+++ patchfinder/src/main/java/fixes/Fix.java
@@ -25,9 +25,97 @@ package fixes;
  */
 
 
+import java.util.ArrayList;
+
 /**
  * Model class for fixes found by fixfinder
  */
 public class Fix {
     // TODO: This
+    private final String fix_id;
+    private final String fix_url;
+    private final String fix_description;
+    private final String source_url_id;
+    //store the source urls
+    private final ArrayList<String> source_urls;
+
+    /**
+     * Model class for fix objects
+     *
+     * @param fix_id         the ID of the fix
+     * @param fix_url        the URL of the fix
+     * @param fix_description the description of the fix
+     * @param source_url_id  the ID of the source URL
+     * @param source_urls    the source URLs
+     */
+    public Fix(String fix_id, String fix_url, String fix_description, String source_url_id, ArrayList<String> source_urls) {
+        super();
+        this.fix_id = fix_id;
+        this.fix_url = fix_url;
+        this.fix_description = fix_description;
+        this.source_url_id = source_url_id;
+        this.source_urls = source_urls;
+    }
+
+    /**
+     * @return the fix_id
+     */
+    public String getFix_id() {
+        return fix_id;
+    }
+
+    /**
+     * @return the fix_url
+     */
+    public String getFix_url() {
+        return fix_url;
+    }
+
+    /**
+     * @return the fix_description
+     */
+    public String getFix_description() {
+        return fix_description;
+    }
+
+    /**
+     * @return the source_url_id
+     */
+    public String getSource_url_id() {
+        return source_url_id;
+    }
+
+    /**
+     * @return the source_urls
+     */
+    public ArrayList<String> getSource_urls() {
+        return source_urls;
+    }
+
+    /**
+     * @return the source_urls as a string
+     */
+    public String getSource_urls_string() {
+        String source_urls_string = "";
+        for (String source_url : source_urls) {
+            source_urls_string += source_url + "\n";
+        }
+        return source_urls_string;
+    }
+
+    /**
+     * set the source_urls
+     */
+    public void setSource_urls(ArrayList<String> source_urls) {
+        this.source_urls.clear();
+        this.source_urls.addAll(source_urls);
+    }
+
+    /**
+     * @return the fix as a string
+     */
+    public String toString() {
+        return "Fix [fix_id=" + fix_id + ", fix_url=" + fix_url + ", fix_description=" + fix_description
+                + ", source_url_id=" + source_url_id + ", source_urls=" + source_urls + "]";
+    }
 }
