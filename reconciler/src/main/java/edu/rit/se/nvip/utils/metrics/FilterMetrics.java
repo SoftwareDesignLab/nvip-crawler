800532f10381e673c9e1df886c56146c66036de3
Chris Enoch <ctevse@rit.edu>
2023-10-20 19:35:04 +0000

    Sorted listFiles() return so that the return is consistent
    - The API for File.listFiles() says that the order of files returned is not guarenteed which could be causing test failures

--- reconciler/src/main/java/edu/rit/se/nvip/utils/metrics/FilterMetrics.java
+++ reconciler/src/main/java/edu/rit/se/nvip/utils/metrics/FilterMetrics.java
@@ -139,6 +139,7 @@ public class FilterMetrics {
             File[] files = directory.listFiles();
 
             if (files != null) {
+                Arrays.sort(files, Comparator.comparing(File::getName));
                 for (File file : files) {
                     if (file.isFile() && file.getName().toLowerCase().endsWith(".json")) {
                         jsonFiles.add(file);
