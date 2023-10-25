5a73870df33cad18c70aa66d34a97b5fb34036b2
Dylan Mulligan <dylan.mulligan2020@gmail.com>
2023-10-25 14:30:54 -0400

    Framed out tests for Fixfinder

---
+++ patchfinder/src/test/java/fixes/parsers/CISAParserTest.java
@@ -0,0 +1,15 @@
+package fixes.parsers;
+
+import org.junit.Test;
+
+public class CISAParserTest extends FixParserTest<CISAParser> {
+    public CISAParserTest() {
+        // TODO: Initialize with test values
+        super(new CISAParser("", ""));
+    }
+
+    @Override
+    public void testParseWebpage() {
+        // TODO: Test parseWebpage
+    }
+}
---
+++ patchfinder/src/test/java/fixes/urlfinders/NvdFixUrlFinderTest.java
@@ -0,0 +1,12 @@
+package fixes.urlfinders;
+
+public class NvdFixUrlFinderTest extends FixUrlFinderTest<NvdFixUrlFinder> {
+    public NvdFixUrlFinderTest() {
+        super(new NvdFixUrlFinder());
+    }
+
+    @Override
+    public void testRun() {
+        // TODO: Test run
+    }
+}
