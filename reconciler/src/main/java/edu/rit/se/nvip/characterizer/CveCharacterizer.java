c4069d4769c096298584e796d60d567e3ff1393d
Chris Enoch <ctevse@rit.edu>
2023-11-12 21:27:14 +0000

    Added envvars for SSVC API

--- reconciler/src/main/java/edu/rit/se/nvip/characterizer/CveCharacterizer.java
+++ reconciler/src/main/java/edu/rit/se/nvip/characterizer/CveCharacterizer.java
@@ -59,7 +59,7 @@ import java.util.*;
 public class CveCharacterizer {
 	private Logger logger = LogManager.getLogger(CveCharacterizer.class.getSimpleName());
 	private final Map<VDONounGroup, AbstractCveClassifier> nounGroupToClassifier = new HashMap<>();
-	private final static ObjectMapper OM = new ObjectMapper();
+	private ObjectMapper OM = new ObjectMapper();
 	private final DatabaseHelper dbh;
 
 	/**
@@ -69,6 +69,10 @@ public class CveCharacterizer {
 	private PartialCvssVectorGenerator partialCvssVectorGenerator;
 	private CvssScoreCalculator cvssScoreCalculator;
 	private CvePreProcessor cvePreProcessor;
+
+	private String SSVCApiBaseUrl = "http://localhost";
+	private String SSVCApiPort = "5000";
+	private String SSVCApiUri = "/ssvc";
 
 	/**
 	 * Construct a CVE Characterizer. You need to provide an initial training data
@@ -251,7 +255,7 @@ public class CveCharacterizer {
 			params.put("exploitStatus", dbh.exploitExists(vuln.getCveId()) ? "POC" : "NONE");
 
 			// Create url object
-			final URL url = new URL("http://localhost:5000/ssvc" + getParamsString(params));
+			final URL url = new URL(this.getSSVCUrl() + getParamsString(params));
 
 			// Build object from request response
 			return OM.readValue(url, SSVC.class);
@@ -297,5 +301,25 @@ public class CveCharacterizer {
 		return resultString.length() > 0
 				? resultString.substring(0, resultString.length() - 1)
 				: resultString;
+	}
+
+	private String getSSVCUrl(){
+		return String.format("%s:%s%s", SSVCApiBaseUrl, SSVCApiPort, SSVCApiUri);
+	}
+
+	public void setSSVCApiBaseUrl(String url){
+		this.SSVCApiBaseUrl = url;
+	}
+
+	public void setSSVCApiPort(String port){
+		this.SSVCApiPort = port;
+	}
+
+	public void setSSVCApiUri(String uri){
+		this.SSVCApiUri = uri;
+	}
+
+	public void setObjectMapper(ObjectMapper mapper){
+		this.OM = mapper;
 	}
 }
--- reconciler/src/test/java/edu/rit/se/nvip/characterizer/CveCharacterizerTest.java
+++ reconciler/src/test/java/edu/rit/se/nvip/characterizer/CveCharacterizerTest.java
@@ -23,36 +23,35 @@ package edu.rit.se.nvip.characterizer;
  * SOFTWARE.
  */
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.rit.se.nvip.DatabaseHelper;
 import edu.rit.se.nvip.automatedcvss.CvssScoreCalculator;
 import edu.rit.se.nvip.automatedcvss.PartialCvssVectorGenerator;
 import edu.rit.se.nvip.automatedcvss.preprocessor.CvePreProcessor;
-import edu.rit.se.nvip.characterizer.classifier.AbstractCveClassifier;
 import edu.rit.se.nvip.characterizer.classifier.CveClassifierFactory;
 import edu.rit.se.nvip.characterizer.classifier.OrdinaryCveClassifier;
 import edu.rit.se.nvip.characterizer.enums.VDOLabel;
-import edu.rit.se.nvip.characterizer.enums.VDONounGroup;
+import edu.rit.se.nvip.model.CompositeDescription;
 import edu.rit.se.nvip.model.CompositeVulnerability;
 import edu.rit.se.nvip.model.RawVulnerability;
+import edu.rit.se.nvip.model.SSVC;
 import edu.rit.se.nvip.utils.CsvUtils;
 import edu.rit.se.nvip.utils.ReconcilerEnvVars;
 import org.apache.commons.io.FileUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.mockito.MockedConstruction;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.stubbing.Answer;
-import weka.classifiers.Classifier;
-import weka.classifiers.bayes.NaiveBayes;
 
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.*;
 import java.nio.file.Paths;
 import java.io.File;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class CveCharacterizerTest {
@@ -133,5 +132,75 @@ public class CveCharacterizerTest {
 		assertEquals(12, vulnSet.size());
 
 		mockedUtils.close();
+	}
+
+	@Nested
+	public class CharacterizeCveListTests {
+
+		@Test
+		void itCallsSSVCApi(){
+			//mocks
+			MockedStatic<FileUtils> mockedUtils = mockStatic(FileUtils.class);
+			CvePreProcessor mockPreProcessor = mock(CvePreProcessor.class);
+			CveClassifierFactory mockCveClassifierFactory = mock(CveClassifierFactory.class);
+			CvssScoreCalculator mockCvssScoreCalculator = mock(CvssScoreCalculator.class);
+			PartialCvssVectorGenerator mockPartialCvssVectorGenerator = mock(PartialCvssVectorGenerator.class);
+			OrdinaryCveClassifier mockClassifier = mock(OrdinaryCveClassifier.class);
+			String[] trainingDataInfo = {ReconcilerEnvVars.getTrainingDataDir(), ReconcilerEnvVars.getTrainingData()};
+			//dummy predictions
+			ArrayList<String[]> dummyPredictions = new ArrayList<>();
+			dummyPredictions.add(new String[]{"Local", "0.8"});
+			dummyPredictions.add(new String[]{"Read", "0.1"});
+			dummyPredictions.add(new String[]{"Remote", "2.1"});
+			dummyPredictions.add(new String[]{"Write", "1.4"});
+			dummyPredictions.add(new String[]{"Privilege Escalation", "1.5"});
+			dummyPredictions.add(new String[]{"Physical", "1.0"});
+			double[] dummyDoubles = {2.0, 1.0, 3.0, 1.0};
+			// test prediction
+			String cveDesc = "7.2 HIGH9.0 HIGHCVE-2020-11544 Ã¢â‚¬â€� An issue was discovered in Project Worlds Official Car Rental System 1. It allows the admin user to run commands on the server with their account because the upload section on the file-manager page contains an arbitrary file upload vulnerability via... read CVE-2020-11544 Published: April 06, 2020; 12:15:13 PM -04:00 CVE-2020-11544read CVE-2020-11544V3.1:7.2 HIGH6.5 MEDIUM";
+			//mock actual calls
+			when(mockPreProcessor.preProcessFile(anyString())).thenReturn("mocked, content");
+			when(mockPreProcessor.preProcessLine(anyString())).thenReturn("mocked, content");
+			when(mockCveClassifierFactory.getCveClassifier(anyString(), anyString(), anyString())).thenReturn(mockClassifier);
+			doNothing().when(mockClassifier).setCveClassifierName(anyString());
+			doNothing().when(mockClassifier).trainMLModel();
+			mockedUtils.when(() -> FileUtils.readFileToString(any(File.class))).thenReturn("{\"key\" : \"value\"}");
+			mockedUtils.when(() -> FileUtils.writeStringToFile(any(File.class), anyString(), anyBoolean())).thenAnswer((Answer<Void>) invocation -> null);
+			when(mockClassifier.predict(anyString(), anyBoolean())).thenReturn(dummyPredictions);
+			when(mockPartialCvssVectorGenerator.getCVssVector(anySet())).thenReturn(new String[8]);
+			//create characterizer with the mocks manually injected
+			CveCharacterizer cveCharacterizer = new CveCharacterizer(mockPreProcessor, mockCveClassifierFactory, mockCvssScoreCalculator, mockPartialCvssVectorGenerator,
+					trainingDataInfo[0], trainingDataInfo[1], "ML", "NB", mock(DatabaseHelper.class)); // TODO: Add/mock dbh
+
+
+			CompositeVulnerability vulnerability = new CompositeVulnerability(new RawVulnerability(1, "CVE-1234-5678", "Buffer overflow in NFS mountd gives root access to remote attackers, mostly in Linux systems.", null, null, null, ""));
+			vulnerability.setPotentialSources(new HashSet<>());
+
+			cveCharacterizer.setSSVCApiBaseUrl("http://test.host");
+
+			ObjectMapper om = mock(ObjectMapper.class);
+			try {
+				when(om.readValue(any(URL.class), eq(SSVC.class))).thenReturn(new SSVC());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			cveCharacterizer.setObjectMapper(om);
+
+			cveCharacterizer.characterizeCveList(Set.of(vulnerability), 100);
+
+			try {
+				verify(om, times(1)).readValue(
+						eq(new URL(String.format(
+								"http://test.host:5000/ssvc?exploitStatus=%s&cveId=%s&description=%s",
+								"NONE",
+								vulnerability.getCveId(),
+								"Buffer+overflow+in+NFS+mountd+gives+root+access+to+remote+attackers%2C+mostly+in+Linux+systems."
+						))),
+						eq(SSVC.class));
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
 	}
 }
