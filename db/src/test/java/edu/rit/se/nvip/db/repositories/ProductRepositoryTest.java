ec4cffe0415819c7f790a68a8a965be421975196
memeeerit <memeee@rit.edu>
2023-11-21 11:32:30 -0500

    updated product repo tests. in the original PNE tests most had the @Test annotation commented to exclude from running and I left those commented

--- db/src/test/java/edu/rit/se/nvip/db/repositories/ProductRepositoryTest.java
+++ db/src/test/java/edu/rit/se/nvip/db/repositories/ProductRepositoryTest.java
@@ -1,71 +1,112 @@
 package edu.rit.se.nvip.db.repositories;
 
-import org.junit.Test;
+import edu.rit.se.nvip.db.model.AffectedProduct;
+import edu.rit.se.nvip.db.model.CpeCollection;
+import edu.rit.se.nvip.db.model.CpeGroup;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class ProductRepositoryTest {
 
-    // todo update these tests
-    //	/**
-    //	 * Tests the insertAffectedProducts method. In this case since there are 5 products,
-    //	 * there should be 8 psmt.setStrings() so 8x5=40
-    //	 *
-    //	 * @throws SQLException
-    //	 */
-    ////	@Test
-    //	public void insertAffectedProductsTest() {
-    //		int inCount = 5;
-    //		List<AffectedProduct> products = buildDummyProducts(inCount);
-    //		dbh.insertAffectedProducts(new CpeCollection(null, products));
-    //		try {
-    //			verify(pstmt, times(inCount*7)).setString(anyInt(), any());
-    //			verify(pstmt, times(inCount)).executeUpdate();
-    //			verify(pstmt).setString(1, products.get(inCount-1).getCveId());
-    //		} catch (SQLException ignored) {}
-    //	}
+    @Mock
+    DataSource dataSource;
+    @Mock
+    Connection mockConnection;
+    @Mock
+    PreparedStatement mockPS;
+    @Mock
+    ResultSet mockRS;
+
+    ProductRepository repository;
 
 
-//    //	@Test
-//    public void testInsertAffectedProductsToDB() {
-//        //dont actually want to insert anything into the db
-//        dbh = spy(dbh);
-//        doNothing().when(dbh).insertAffectedProducts(any());
-//        dbh.insertAffectedProductsToDB(new ArrayList<>());
-//        verify(dbh).insertAffectedProducts(any());
-//    }
 
-//    //	@Test
-//    public void deleteAffectedProductsTest() {
-//        int count = 5;
-//        List<AffectedProduct> products = buildDummyProducts(count);
-//        dbh.deleteAffectedProducts(products);
-//        try {
-//            verify(pstmt, times(count)).setString(anyInt(), any());
-//            verify(pstmt, times(count)).executeUpdate();
-//            verify(pstmt).setString(1, products.get(count-1).getCveId());
-//        } catch (SQLException ignored) {}
-//    }
-//private List<AffectedProduct> buildDummyProducts(int count) {
-//    List<AffectedProduct> products = new ArrayList<>();
-//    for (int i = 0; i < count; i++) {
-//        String cpeName = "cpe:2.3:a:" + i + ":" + i + ":*:*:*:*:*:*:*:*";
-//        products.add(new AffectedProduct("cve"+i, cpeName, "productName"+i, "version"+i, "vendor"+i));
-//    }
-//    return products;
-//}
+    @SneakyThrows
+    @BeforeEach
+    void initializeMocks(){
+        when(mockPS.executeQuery()).thenReturn(mockRS);
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockPS);
+        when(dataSource.getConnection()).thenReturn(mockConnection);
 
-//    @Test
-//    public void testGetAffectedProducts() {
-//        Map<String, CpeGroup> affectedProducts = databaseHelper.getAffectedProducts(null);
-//        assertNotNull(affectedProducts);
-//    }
+        repository = new ProductRepository(dataSource);
+    }
+
+    private List<AffectedProduct> buildDummyProducts(int count) {
+        List<AffectedProduct> products = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            String cpeName = "cpe:2.3:a:" + i + ":" + i + ":*:*:*:*:*:*:*:*";
+            products.add(new AffectedProduct("cve"+i, cpeName, "productName"+i, "version"+i, "vendor"+i));
+        }
+        return products;
+    }
+
+    /**
+     * Tests the insertAffectedProducts method. In this case since there are 5 products,
+     * there should be 8 psmt.setStrings() so 8x5=40
+     *
+     * @throws SQLException
+     */
+    //@Test
+    @SneakyThrows
+    public void insertAffectedProductsTest() {
+        int inCount = 5;
+        List<AffectedProduct> products = buildDummyProducts(inCount);
+        repository.insertAffectedProducts(new CpeCollection(null, products));
+        try {
+            verify(mockPS, times(inCount*7)).setString(anyInt(), any());
+            verify(mockPS, times(inCount)).executeUpdate();
+            verify(mockPS).setString(1, products.get(inCount-1).getCveId());
+        } catch (SQLException ignored) {}
+    }
+
+
+    //	@Test
+    public void testInsertAffectedProductsToDB() {
+        //dont actually want to insert anything into the db
+        repository = spy(repository);
+        doNothing().when(repository).insertAffectedProducts(any());
+        repository.insertAffectedProductsToDB(new ArrayList<>());
+        verify(repository).insertAffectedProducts(any());
+    }
+
+    //	@Test
+    public void deleteAffectedProductsTest() {
+        int count = 5;
+        List<AffectedProduct> products = buildDummyProducts(count);
+        repository.deleteAffectedProducts(products);
+        try {
+            verify(mockPS, times(count)).setString(anyInt(), any());
+            verify(mockPS, times(count)).executeUpdate();
+            verify(mockPS).setString(1, products.get(count-1).getCveId());
+        } catch (SQLException ignored) {}
+    }
+
+    @Test
+    public void testGetAffectedProducts() {
+        Map<String, CpeGroup> affectedProducts = repository.getAffectedProducts(null);
+        assertNotNull(affectedProducts);
+    }
 
 }
